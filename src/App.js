9514f6837b2235d5249535e291df0e738487d964
Seth Archer <sethparcher@gmail.com>
2025-08-22 16:19:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -202,8 +202,7 @@ export default function App() {
         </div>
       </header>
 
-      <main className="max-w-7xl mx-auto p-4 grid grid-cols-1 lg:grid-cols-12 gap-4">
-        {/* Controls */}
+    <main className="max-w-7xl mx-auto p-4 grid grid-cols-1 lg:grid-cols-12 gap-4 lg:items-start">        {/* Controls */}
         <section className="lg:col-span-4">
           <div className="bg-white rounded-2xl shadow-sm border border-neutral-200 p-4 space-y-4">
             <div>
